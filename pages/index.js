a602739e7d0497a4805e015de28343acaf282ce1
Olaurito Neto <linkinneto.bsi@gmail.com>
2022-11-09 23:08:51 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,102 @@
+import config from "../config.json";
+import styled from "styled-components";
+import {CSSReset} from "../src/components/CSSRReset";
+import Menu from "../src/components/Menu";
+import {StyledTimeline} from "../src/components/Timeline";
+
+function HomePage() {
+    // const mensagem = "Bem vindo ao AluraTube!";
+    const estilosDaHomePage = {
+        // backgroundColor: "red"
+    };
+
+    console.log(config.playlists);
+
+    return (
+        <>
+            <CSSReset/>
+
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+                // backgroundColor: "red",
+            }}>
+                <Menu/>
+                <Header/>
+                <Timeline playlists={config.playlists}>
+
+                </Timeline>
+            </div>
+        </>
+    )
+}
+
+export default HomePage
+
+/*function Menu() {
+    return (
+        <div>
+            Menu
+        </div>
+    )
+}*/
+
+const StyleHeader = styled.div`
+img{
+    width: 80px;
+    height: 80px;
+    border-radius
+}
+
+.user-info{
+margin-top: 50px;
+display: flex; align-items:center; width: 100%; padding: 16px 32px; gap:18px ;
+}
+
+`;
+
+function Header() {
+    return (
+        <StyleHeader>
+            {/*<img src="banner"/>*/}
+            <section className="user-info">
+                <img src={`https://github.com/${config.github}.png`}/>
+                <div>
+                    <h2>{config.name}</h2>
+                    <p>{config.job}</p>
+                </div>
+            </section>
+        </StyleHeader>
+    )
+}
+
+function Timeline(props) {
+    console.log("Dentro do componente", props.playlists);
+    const playlistNames = Object.keys(props.playlists);
+
+    return (
+        <StyledTimeline>
+            {playlistNames.map(playlistName => {
+                const videos = props.playlists[playlistName];
+                return (
+                    <section>
+                        <h2>{playlistName}</h2>
+                        <div>
+                            {videos.map((video) => {
+                                return (
+                                    <a href={video.url}>
+                                        <img src={video.thumb}/>
+                                        <span>
+                        {video.title}
+                            </span>
+                                    </a>
+                                )
+                            })}
+                        </div>
+                    </section>
+                )
+            })}
+        </StyledTimeline>
+    )
+}
